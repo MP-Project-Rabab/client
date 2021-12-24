44007f8a39fb0760223dbff06001680be16bd9d8
rabab-1993 <rabab.abdulaziz.elq.js@tuwaiq.edu.sa>
2021-12-25 00:11:46 +0300

    create Forget passowrd Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Component/Home/Home";
 import Register from "./Component/Account/Register/Register";
 import Login from "./Component/Account/Login/Login";
+import Forget from "./Component/Account/ForgetPass/ForgetPass";
 
 import "./App.css";
 
@@ -13,10 +14,10 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/register" element={<Register />} />
          <Route exact path="/login" element={<Login />} />
+        <Route exact path="/forget" element={<Forget />} />
         {/* <Route exact path="/posts" element={<Post />} /> */}
         {/* <Route exact path="/profile" element={<Profile />} /> */}
         {/* <Route exact path="/activate/" element={<Activate />} /> */}
-        {/* <Route exact path="/forget" element={<Forget />} /> */}
         {/* <Route exact path="/reset-pass/:id" element={<ResetPass />} />  */}
       </Routes>
     </div>
--- src/Component/Account/ForgetPass/ForgetPass.js
+++ src/Component/Account/ForgetPass/ForgetPass.js
@@ -1,12 +1,53 @@
-import React from 'react'
-import './style.css'
-
+import React, { useState } from "react";
+import axios from "axios";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import "./style.css";
 const ForgetPass = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [msg, setMsg] = useState("");
+  const forgetPass = async () => {
+    console.log(email);
+    try {
+      const result = await axios.put(
+        `${process.env.REACT_APP_BASE_URL}/user/forget`,
+        { email }
+      );
 
-export default ForgetPass
+      console.log(result);
+      setMsg("Check your Email for resetting your passowrd");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <Container>
+        <Stack spacing={5} className="register-form">
+          <h1>Enter your email</h1>
+          {/* Email Field */}
+          <TextField
+            required
+            id="standard-required"
+            label="Email"
+            type="email"
+            variant="standard"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
+
+          <Button appearance="primary" onClick={forgetPass}>
+            Submit
+          </Button>
+
+          <h1>{msg}</h1>
+        </Stack>
+      </Container>
+    </div>
+  );
+};
+
+export default ForgetPass;
